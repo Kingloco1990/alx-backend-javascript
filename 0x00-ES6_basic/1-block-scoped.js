0760db7e4b23d4fd9248c2d8874f98b29fbc76ef
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-04-25 23:27:28 +0000

    Modify function so that variables aren't overwritten

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
-    /* eslint-disable no-unused-vars */
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
