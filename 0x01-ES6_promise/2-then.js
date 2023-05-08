ca1ba4d3a3dcf18b33f672e8e5935fd3d74d143d
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-08 05:58:55 +0000

    Appends three handlers to a function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   const resolveObject = {
-	  status: 200,
-	  body: 'success'
+    status: 200,
+    body: 'success',
   };
 
   return promise
-    .then(() =>  resolveObject)
+    .then(() => resolveObject)
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
