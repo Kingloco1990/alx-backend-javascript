7860055f32dcdfe50f434f03ea559d4731021f38
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-12 15:46:14 +0000

    Returns an object with a specific format

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,15 +1,28 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
+  let returnValue1;
+  let returnValue2;
+
   try {
-    return {
-      photo: await uploadPhoto(),
-      user: await createUser(),
-    };
-  } catch (err) {
-    return {
-      photo: null,
-      user: null,
-    };
+    returnValue1 = await uploadPhoto();
+  } catch (error) {
+    returnValue1 = 'null';
   }
+
+  try {
+    returnValue2 = await createUser();
+  } catch (error) {
+    returnValue2 = 'null';
+  }
+
+  if (returnValue1 === 'null' || returnValue2 === 'nul') {
+    return {
+      photo: returnValue1,
+      user: returnValue2,
+    };
+  } return {
+    photo: returnValue1,
+    user: returnValue2,
+  };
 }
