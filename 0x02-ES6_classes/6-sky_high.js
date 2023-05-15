94c971b7ef4d266725def126c813fe1a8e73fd50
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-15 20:00:21 +0000

    Commit for the 0x02. ES6 classes project

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -4,20 +4,13 @@ export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     // call constructor of super class (Building)
     super(sqft);
-
-    // Create objs
     this._floors = floors;
   }
-
-  // Methods
 
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
 
-  // Setters
-
-  // Getters
   get sqft() {
     return this._sqft;
   }
