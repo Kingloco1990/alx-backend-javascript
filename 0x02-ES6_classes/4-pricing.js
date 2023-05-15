1bc529a5dd38c253802f763630139e14cc7c01d8
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-15 19:53:40 +0000

    Commit for the 0x02. ES6 classes project

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,12 +2,9 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    // Create objs
     this._amount = amount;
     this._currency = currency;
   }
-
-  // Methods
 
   displayFullPrice() {
     return (`${this.amount} ${this.currency.name} (${this.currency.code})`);
@@ -17,22 +14,21 @@ export default class Pricing {
     return amount * conversionRate;
   }
 
-  // Setters
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  set currency(newCurrency) {
-    if (!(newCurrency instanceof Currency)) throw TypeError('currency must be an instance of Currency');
-    this._currency = newCurrency;
-  }
-
-  // Getters
   get amount() {
     return this._amount;
   }
 
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
   get currency() {
     return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw TypeError('currency must be an instance of Currency');
+    } this._currency = newCurrency;
   }
 }
