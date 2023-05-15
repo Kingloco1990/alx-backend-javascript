84fc5817c98341efc927f1821b257081bb5f3a4e
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-15 10:24:14 +0000

    Commit for the 0x02. ES6 classes project

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,31 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    // Create objs
     this._code = code;
     this._name = name;
   }
-
-  // Methods
 
   displayFullCurrency() {
     return (`${this.name} (${this.code})`);
   }
 
-  // Setters
-  set name(newName) {
-    this._name = newName;
-  }
-
-  set code(newCode) {
-    this._name = newCode;
-  }
-
-  // Getters
   get name() {
     return this._name;
   }
 
+  set name(newName) {
+    this._name = newName;
+  }
+
   get code() {
     return this._code;
+  }
+
+  set code(newCode) {
+    this._name = newCode;
   }
 }
