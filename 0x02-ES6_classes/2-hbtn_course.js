f8f5502aa367c2f7cd179a6ce146d9114c01307c
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-15 10:07:58 +0000

    Commit for the 0x02. ES6 classes project

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,9 +10,8 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string') {
-      throw TypeError('Name must be a string');
-    } this._name = name;
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
   }
 
   get length() {
@@ -20,9 +19,8 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length !== 'number') {
-      throw TypeError('Length must be a number');
-    } this._length = length;
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
   }
 
   get students() {
@@ -30,8 +28,11 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (Array.isArray(students) !== true || students.every((element) => typeof element !== 'string')) {
-      throw TypeError('Students must be an array of strings');
-    } this._students = students;
+    if (
+      !Array.isArray(students)
+      || students.every((student) => typeof student !== 'string')
+    ) throw TypeError('Students must be an array of strings');
+
+    this._students = students;
   }
 }
