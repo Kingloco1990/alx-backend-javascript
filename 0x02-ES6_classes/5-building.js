878ed0ef6bc4c4fbc60fb074483418071f6c764f
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-15 19:58:28 +0000

    Commit for the 0x02. ES6 classes project

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    // Any class extending Building must implement below method
-    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-
-    // Create objs
-    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    } this._sqft = sqft;
   }
 
-  // Methods
-
-  // Setters
-
-  // Getters
   get sqft() {
     return this._sqft;
   }
