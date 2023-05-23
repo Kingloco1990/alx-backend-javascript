1ba62bb0bdd3243fd2916cbffa218feeaa6b992b
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-23 23:15:35 +0000

    Commit for tasks 1 to 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,7 +4,7 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: boolean;
+  [propName: string]: any;
   }
 
 interface Directors extends Teacher {
