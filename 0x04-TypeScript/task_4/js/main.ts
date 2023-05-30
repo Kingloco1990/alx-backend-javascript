c577f1c9d78a59804332088209579b6db69902ca
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-30 11:47:45 +0000

    Commit for task 4

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,23 +1,24 @@
 export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
+
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Dennis',
-  lastName: 'Ritchie',
+  firstName: 'King',
+  lastName: 'Loco',
   experienceTeachingC: 10,
-};
+}
 
 console.log('C++');
-cpp.setTeacher = cTeacher;
+cpp.teacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
 console.log('Java');
-java.setTeacher = cTeacher;
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
+cpp.teacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher())
 
 console.log('React');
-react.setTeacher = cTeacher;
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+cpp.teacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher())
--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,15 +1,15 @@
 namespace Subjects {
   export interface Teacher {
-    experienceTeachingReact?: number;
+    experienceTeachingJava?: number;
   }
 
-  export class React extends Subject {
+  export class Java extends Subject {
     getRequirements = (): string => {
-      return 'Here is the list of requirements for React';
+      return 'Here is the list of requirements for Java';
     }
 
     getAvailableTeacher = (): string => {
-      if (this.teacher.experienceTeachingReact > 0) {
+      if (this.teacher.experienceTeachingJava > 0) {
         return `Available Teacher: ${this.teacher.firstName}`;
       }
       return 'No available teacher'; 
