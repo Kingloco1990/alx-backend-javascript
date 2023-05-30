3dd8bba7469a116d0e5c16f34e2d78294de32a57
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-05-30 11:47:10 +0000

    Commit for task 3

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,6 +1,4 @@
-
-import {RowElement,RowID } from './interface';
-
+import { RowElement, RowID } from './interface.ts';
 
 declare function insertRow(row: RowElement): number;
 declare function deleteRow(rowId: RowID): void;
